e5c692e0dfc052a182804166086ea98d6142d1e8
Marvel957 <hamsik957@gmail.com>
2022-06-02 16:18:23 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#
+#include <unistd.h>
 
 
 /**
@@ -7,4 +7,9 @@
  *
  * Return: Always 0 (success)
  */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
 
