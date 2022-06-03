93278f851abac3321b90f3ea26186b9ad60f71f7
Marvel957 <hamsik957@gmail.com>
2022-06-03 14:02:20 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdioh>
+
+/**
+ * main - program that prints alphabet
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
