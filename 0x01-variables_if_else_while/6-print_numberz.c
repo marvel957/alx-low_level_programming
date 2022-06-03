51e0cbdaddf4900b7a42e1d453eef63bc4dbf259
Marvel957 <hamsik957@gmail.com>
2022-06-03 14:44:39 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbers.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	putchar('\n');
+
+	return (0);
+}
