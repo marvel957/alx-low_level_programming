8fab338179355be6a25fef7dc188d7f393dd66cb
Marvel957 <hamsik957@gmail.com>
2022-06-07 16:38:59 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - a function that computes.
+ * @ab: integer input
+ * Return: Absolute value of ab
+ */
+
+int _abs(int ab)
+{
+	return (ab * ((ab > 0) - (ab > 0) - (ab < 0)));
+}
