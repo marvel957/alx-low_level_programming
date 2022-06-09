9bf62c5a0982329204861bad3de3cd0c0f41d170
Marvel957 <hamsik957@gmail.com>
2022-06-09 08:30:39 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - function name
+ *
+ * Return: Void.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar("\n");
+
+	return (0);
+
+}
