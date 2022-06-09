0d9cc67467ae9e6f87981e05281e0097a0e41ebf
Marvel957 <hamsik957@gmail.com>
2022-06-09 10:55:03 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - a printing func
+ * @n: input number amount of times
+ * Return: a straight line
+ */
+void print_line(int n)
+{
+	int co;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (co = 1; co <= n; co++)
+		{
+			_putchar('_');
+		}
+	}
+}
