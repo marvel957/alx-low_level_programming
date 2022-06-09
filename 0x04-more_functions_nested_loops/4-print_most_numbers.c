8a223204f9e883f6a00ff9571695cfcd673863a0
Marvel957 <hamsik957@gmail.com>
2022-06-09 09:28:26 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,9 +11,9 @@ void print_most_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		if(( c != '2')  && (c != '4'))
+		if ((c != '2')  && (c != '4'))
 		{
-			_putchar(i);
+			_putchar(c);
 		}
 	}
 	_putchar('\n');
