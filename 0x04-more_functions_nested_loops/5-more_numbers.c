4151aa7376b76c2ce37529ca84c50b79b2c5c85b
Marvel957 <hamsik957@gmail.com>
2022-06-09 10:21:44 -0700

    task5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers- function mame
+ *
+ * Rwturn: 0 or 1
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		if (i >= 10)
+		{
+			_putchar((i / 10) + '0');
+		}
+		_putchar((i % 10) + '0');
+	}
+	_putchar('\n');
+}
