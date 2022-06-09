95c3a185ee8b8f682150e46437da0fc388daec59
Marvel957 <hamsik957@gmail.com>
2022-06-09 08:10:58 -0700

    task2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,7 +4,7 @@
  * mul - A function that multiplies 2 number
  * @a: parameter 1
  * @b: parameter 2
- * Return : return mul of 2 numbers
+ * Return: return mul of 2 numbers
  */
 int mul(int a, int b)
 {
