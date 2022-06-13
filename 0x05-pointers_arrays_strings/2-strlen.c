53c7a49b390691143bb1e62cae9d902ae02fc5e0
Marvel957 <hamsik957@gmail.com>
2022-06-13 06:24:49 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#imclude "main.h"
+
+/**
+ * _strlen-function name
+ * @s:parameter
+ * Return: Rubish
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+		;
+	return (i);
+}
+
