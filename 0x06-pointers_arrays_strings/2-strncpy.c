500b0dc2f5410d4c760568d28647cf2666446130
Marvel957 <hamsik957@gmail.com>
2022-06-16 03:49:45 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncpy - func name
+ * @dest: param 1
+ * @n: param 2
+ * @n: max bytes
+ * Return: returns a pointer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, desti = 0;
+
+	while (src[i++])
+		desti++;
+	for (i = 0; src[i] && i < n; i++)
+		dest[desti++] = src[i];
+	for (i = desti; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}
