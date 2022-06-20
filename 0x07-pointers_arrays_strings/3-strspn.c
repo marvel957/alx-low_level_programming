fe767c779ceef2780f0d95a1346c580f50c719b3
Marvel957 <hamsik957@gmail.com>
2022-06-20 07:32:19 -0700

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strspn - func name
+ * @s: param 1
+ * @accept: param 2
+ * Return: return value
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int len = 0;
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				len++;
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+				return(len);
+		}
+		s++;
+	}
+	return(len);
+}
+
