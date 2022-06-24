4f0241e3509deeac48d47ff4dc5cae22d951aad6
Marvel957 <hamsik957@gmail.com>
2022-06-24 10:25:38 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - func name
+ * @argc:param 1
+ * @argv: param 2
+ * Return:  0.
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
