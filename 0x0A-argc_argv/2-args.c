e080ac760d7fde18830e59beaf409a7974c04a81
Marvel957 <hamsik957@gmail.com>
2022-06-24 10:41:09 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - func name
+ * @argc: param 1
+ * @argv: param 2
+ * Return: 0 
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
