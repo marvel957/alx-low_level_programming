91fb5e04f0ce74b6aa7951b7c3334ae269d2c98f
Marvel957 <hamsik957@gmail.com>
2022-06-24 10:37:25 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - func name
+ * @argc: number of command line arguments
+ * @argv: param 2
+ * Return: a value
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
