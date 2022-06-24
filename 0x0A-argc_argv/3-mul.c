92182eaa02596451ba2c450ed969dcdc00935c81
Marvel957 <hamsik957@gmail.com>
2022-06-24 10:51:58 -0700

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - fumc name
+ * @argc: arg counter
+ * @argv: argument variable
+ * Return:a value
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]);
+	return (0);
+}
